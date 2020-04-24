50f10729863e6410b3a55e4010ee2f45da62b358
Igor Oliveira <schreiber2011@gmail.com>
2020-04-24 11:27:15 -0400

    Test mode

--- src/test/java/com/kazale/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/com/kazale/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -1,13 +1,18 @@
 package com.kazale.pontointeligente.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class PontoInteligenteApplicationTests {
+@ActiveProfiles("test")
+public class PontoInteligenteApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
